f54caa9d8bcd951c9fdd3af977a990f260347391
Nathaniel Walston <nathaniel.walston@kareo.com>
2020-01-26 12:50:14 -0800

    Tried to port logic to new keyboard

---
+++ keyboards/ymdk_sp64/keymaps/default/keymap.c
@@ -0,0 +1,151 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+
+#define BASE 0 // default layer
+#define LFUN 1 // function layer
+#define LNUM 2 // num-pad
+#define LMDA 3 // media keys
+
+enum custom_keycodes {
+#ifdef ORYX_CONFIGURATOR
+  EPRM = EZ_SAFE_RANGE,
+#else
+  EPRM = SAFE_RANGE,
+#endif
+  VRSN,
+  RGB_SLD
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Keymap 0: Basic layer
+ *d
+ * ┌-----┬-----┬-----┬-----┬-----┬-----┬-----┐       ┌-----┬-----┬-----┬-----┬-----┬-----┬----------┐
+ * │  `  │  1  │  2  │  3  │  4  │  5  │  6  │       │  7  │  8  │  9  │  0  │  -  │  =  │ BkSp     │
+ * ├-----┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┘    ┌--┴-----┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬-------┤
+ * │ Tab    │  Q  │  W  │  E  │  R  │  T  │       │  Y  │  U  │  I  │  O  │  P  │  [  │  ]  │ \     │
+ * ├--------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴-------┤
+ * │ Ctrl    │  A  │  S  │  D  │  F  │  G  │       │  H  │  J  │  K  │  L  │  ;  │  '  │ Enter      │
+ * ├---------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬-----┬-----┤
+ * │ LShift   │  Z  │  X  │  C  │  V  │  B  │       │  N  │  M  │  <  │  >  │  ?  │Shift│ Up  │ Ly3 │
+ * ├------┬---┴-┬---┴--┬--┴-----┴----┬┴----┬┘      ┌┴-----┴-----┴--┬--┴----┬┴-----┼-----┼-----┼-----┤
+ * │ Ly2  │ Alt │ Cmd  │             │ Ly1 │       │               │ Cmd   │ Alt  │ Left│ Down│Right│
+ * └------┴-----┴------┴-------------┴-----┘       └---------------┴-------┴------┴-----┴-----┴-----┘
+ */
+[BASE] = LAYOUT_sp64(
+  // left hand
+  MT(RESET, KC_GRAVE), KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_6,
+  KC_TAB,              KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,
+  KC_LCTL,             KC_A,    KC_S,    KC_D,     KC_F,    KC_G,
+  KC_LSFT,             KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,
+  LT(LNUM, KC_NO),   KC_LALT, KC_LGUI, KC_SPACE, LT(LFUN, KC_SPACE),
+  // right hand
+  KC_7,     KC_8,    KC_9,    KC_0,    KC_MINUS,  KC_EQUAL,                 KC_BSPACE,
+  KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,      KC_LBRACKET,   KC_RBRACKET, KC_BSLS,
+  KC_H,     KC_J,    KC_K,    KC_L,    KC_SCOLON, KC_QUOTE,                  KC_ENTER,
+  KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,     KC_UP, LT(LFUN, KC_NO),
+  KC_SPACE,          KC_RGUI, KC_RALT,            KC_LEFT,   KC_DOWN,        KC_RIGHT
+),
+/* Keymap 1: Function Layer / Vim Arrows
+ *
+ * ┌-----┬-----┬-----┬-----┬-----┬-----┬-----┐       ┌-----┬-----┬-----┬-----┬-----┬-----┬----------┐
+ * │ Esc │ F1  │  F2 │ F3  │ F4  │ F5  │ F6  │       │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │          │
+ * ├-----┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┘    ┌--┴-----┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬-------┤
+ * │        │     │     │     │     │     │       │     │     │     │     │     │     │     │       │
+ * ├--------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴-------┤
+ * │         │     │     │     │     │     │       │     │Left │ Up  │Down │Right│     │            │
+ * ├---------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬-----┬-----┤
+ * │          │     │     │     │     │     │       │     │     │     │     │     │     │     │     │
+ * ├------┬---┴-┬---┴--┬--┴-----┴----┬┴----┬┘      ┌┴-----┴-----┴--┬--┴----┬┴-----┼-----┼-----┼-----┤
+ * │ Ly2  │     │      │             │ Ly1 │       │               │       │      │     │     │     │
+ * └------┴-----┴------┴-------------┴-----┘       └---------------┴-------┴------┴-----┴-----┴-----┘
+ */
+[LFUN] = LAYOUT_sp64(
+  // left hand
+  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+  // right hand
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,           KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, KC_TRNS,          KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS,          KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS
+),
+/* Keymap 2: NumPad
+ *
+ * ┌-----┬-----┬-----┬-----┬-----┬-----┬-----┐       ┌-----┬-----┬-----┬-----┬-----┬-----┬----------┐
+ * │     │     │     │     │     │     │     │       │  7  │  8  │  9  │     │ Vol-│ Vol+│ Mute     │
+ * ├-----┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┘    ┌--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬-------┤
+ * │        │     │     │     │     │     │       │     │  4  │  5  │  6  │     │     │     │       │
+ * ├--------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴-------┤
+ * │         │     │     │     │     │     │       │     │  1  │  2  │  3  │     │     │            │
+ * ├---------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬-----┬-----┤
+ * │          │     │     │     │     │     │       │     │  0  │     │     │     │     │     │ Ly3 │
+ * ├------┬---┴-┬---┴--┬--┴-----┴----┬┴----┬┘      ┌┴-----┴-----┴--┬--┴----┬┴-----┼-----┼-----┼-----┤
+ * │ Ly2  │     │      │             │ Ly1 │       │               │       │      │     │     │     │
+ * └------┴-----┴------┴-------------┴-----┘       └---------------┴-------┴------┴-----┴-----┴-----┘
+ */
+[LNUM] = LAYOUT_sp64(
+  // left hand
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+  // right hand
+  KC_P7,   KC_P8,   KC_P9,   KC_TRNS, KC_VOLU,  KC_VOLD,          KC_MUTE,
+  KC_P4,   KC_P5,   KC_P6,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_P1,   KC_P2,   KC_P3,   KC_DOWN, KC_RIGHT, KC_TRNS,          KC_TRNS,
+  KC_P0,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS,          KC_TRNS, KC_TRNS,           KC_TRNS, KC_TRNS, KC_TRNS
+),
+/* Keymap 3: Media
+ *
+ * ┌-----┬-----┬-----┬-----┬-----┬-----┬-----┐       ┌-----┬-----┬-----┬-----┬-----┬-----┬----------┐
+ * │     │     │     │     │     │     │     │       │     │     │     │     │     │     │          │
+ * ├-----┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┘    ┌--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬-------┤
+ * │        │     │     │     │     │     │       │     │     │     │     │     │     │     │       │
+ * ├--------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴-------┤
+ * │         │     │     │     │     │     │       │     │     │     │     │     │     │            │
+ * ├---------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬-----┬-----┤
+ * │          │     │     │     │     │     │       │     │     │     │     │     │     │     │ Ly3 │
+ * ├------┬---┴-┬---┴--┬--┴-----┴----┬┴----┬┘      ┌┴-----┴-----┴--┬--┴----┬┴-----┼-----┼-----┼-----┤
+ * │ Ly2  │     │      │             │ Ly1 │       │               │       │      │     │     │     │
+ * └------┴-----┴------┴-------------┴-----┘       └---------------┴-------┴------┴-----┴-----┴-----┘
+ */
+[LMDA] = LAYOUT_sp64(
+  // left hand
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+  // right hand
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
+),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case EPRM:
+        eeconfig_init();
+        return false;
+      case VRSN:
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        return false;
+      #ifdef RGBLIGHT_ENABLE
+      case RGB_SLD:
+        rgblight_mode(1);
+        return false;
+      #endif
+    }
+  }
+  return true;
+}
---
+++ keyboards/ymdk_sp64/ymdk_sp64.h
@@ -0,0 +1,90 @@
+/* Copyright 2019 Neil Kettle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
+
+#ifdef RIGHT_HALF
+# include <stdint.h>
+# include "i2c_master.h"
+
+// I2C aliases and register addresses (see "mcp23018.md")
+# define I2C_ADDR        0b0100000
+# define I2C_ADDR_WRITE  ( (I2C_ADDR<<1) | I2C_WRITE )
+# define I2C_ADDR_READ   ( (I2C_ADDR<<1) | I2C_READ  )
+# define IODIRA          0x00            // i/o direction register
+# define IODIRB          0x01
+# define GPPUA           0x0C            // GPIO pull-up resistor register
+# define GPPUB           0x0D
+# define GPIOA           0x12            // general purpose i/o port register (write modifies OLAT)
+# define GPIOB           0x13
+# define OLATA           0x14            // output latch register
+# define OLATB           0x15
+
+extern i2c_status_t mcp23018_status;
+# define MCP23018_I2C_TIMEOUT 100
+
+uint8_t init_mcp23018(void);
+#endif
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+
+/*
+ *  LEFT HAND                                        RIGHT HAND
+ * ┌-----┬-----┬-----┬-----┬-----┬-----┬-----┐       ┌-----┬-----┬-----┬-----┬-----┬-----┬----------┐
+ * │ k00 │ k01 │ k02 │ k03 │ k04 │ k05 │ k06 │       │ k07 │ k08 │ k09 │ k0A │ k0B │ k0C │ k0D      │
+ * ├-----┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┘    ┌--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬--┴--┬-------┤
+ * │ k10    │ k11 │ k12 │ k13 │ k14 │ k15 │       │ k16 │ k17 │ k18 │ k19 │ k1A │ k1B │ k1C │ k1D   │
+ * ├--------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴-------┤
+ * │ k20     │ k21 │ k22 │ k23 │ k24 │ k25 │       │ k26 │ k27 │ k28 │ k29 │ k2A │ k2B │ k2C        │
+ * ├---------┴┬----┴┬----┴┬----┴┬----┴┬----┴┐      └┬----┴┬----┴┬----┴┬----┴┬----┴┬----┴┬-----┬-----┤
+ * │ k30      │ k31 │ k32 │ k33 │ k34 │ k35 │       │ k36 │ k37 │ k38 │ k39 │ k3A │ k3B │ k3C │ k3D │
+ * ├------┬---┴-┬---┴--┬--┴-----┴----┬┴----┬┘      ┌┴-----┴-----┴--┬--┴----┬┴-----┼-----┼-----┼-----┤
+ * │ k40  │ k41 │ k42  │ k43         │ k45 │       │ k46           │ k48   │ k4A  │ k4B │ k4C │ k4D │
+ * └------┴-----┴------┴-------------┴-----┘       └---------------┴-------┴------┴-----┴-----┴-----┘
+ */
+#define LAYOUT_ymdk_sp64(
+    // LEFT HAND
+    k00,k01,k02,k03,k04,k05,k06,     \
+    k10,k11,k12,k13,k14,k15,         \
+    k20,k21,k22,k23,k24,k25,         \
+    k30,k31,k32,k33,k34,k35,         \
+    k40,k41,k42,k43,    k45,         \
+    // RIGHT HAND
+        k07,k08,k09,k0A,k0B,k0C,k0D, \
+    k16,k17,k18,k19,k1A,k1B,k1C,k1D, \
+    k26,k27,k28,k29,k2A,k2B,k2C,     \
+    k36,k37,k38,k39,k3A,k3B,k3C,k3D, \
+    k46,    k48,    k4A,k4B,k4C,k4D, \
+    )                                \
+   /* matrix positions */            \
+   {                                 \
+    { k00,   k01,   k02,   k03,   k04,   k05,   k06,   k07,   k08,   k09,   k0A,   k0B,   k0C,   k0D   },   \
+    { k10,   k11,   k12,   k13,   k14,   k15,   k16,   k17,   k18,   k19,   k1A,   k1B,   k1C,   k1D   },   \
+    { k20,   k21,   k22,   k23,   k24,   k25,   k26,   k27,   k28,   k29,   k2A,   k2B,   k2C,   KC_NO },   \
+    { k30,   k31,   k32,   k33,   k34,   k35,   k36,   k37,   k38,   k39,   k3A,   k3B,   k3C,   k3D   },   \
+    { k40,   k41,   k42,   k43,   KC_NO, k45,   k46,   KC_NO, k48,   KC_NO, k4A,   k4B,   k4C,   k4D   },   \
+   }
+
+#define LAYOUT LAYOUT_sp64
